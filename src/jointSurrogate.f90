60df5f44d212ebeb1165fbb37b69ed7c68757469
socale <cs10@PRT-020.DIM-UB2.local>
2019-06-27 18:09:11 +0200

    Update jointSurrogate.f90

--- src/jointSurrogate.f90
+++ src/jointSurrogate.f90
@@ -327,9 +327,17 @@ subroutine jointsurrogate(nsujet1,ng,ntrials1,maxiter,nst,nparamfrail,indice_a_e
     controlgoto=0
 
     ! initialisation de l'environnement de travail pour le parallelisme MPI 
+<<<<<<< HEAD
     !call MPI_INIT(code) ! cet environnement est desactive a la fin du programme a l'aide de MPI_FINALIZE
     !call MPI_COMM_SIZE(MPI_COMM_WORLD,nb_procs,code) ! commaitre le nombre de processus associe a l'identificateur code
     !call MPI_COMM_RANK(MPI_COMM_WORLD,rang_proc,code)
+=======
+	if(nsim_node(4)==3) then
+		call MPI_INIT(code) ! cet environnement est desactive a la fin du programme a l'aide de MPI_FINALIZE
+		call MPI_COMM_SIZE(MPI_COMM_WORLD,nb_procs,code) ! commaitre le nombre de processus associe a l'identificateur code
+		call MPI_COMM_RANK(MPI_COMM_WORLD,rang_proc,code)
+	endif
+>>>>>>> parent of e18cbf8... update of the R function jointSurroPenalSimul to take into account the Rmpi command
     !desactivation de l'environnement de travail pour le programme parallele
     !!call MPI_FINALIZE(comm) 
 
@@ -3903,9 +3911,15 @@ end do
     1001 continue
     
     !desactivation de l'environnement de travail pour le programme parallele
+<<<<<<< HEAD
     ! if(nsim_node(4).ne.3) then
         ! !call MPI_FINALIZE(comm) 
     ! endif
+=======
+    if(nsim_node(4).ne.3) then
+        call MPI_FINALIZE(code)  
+    endif
+>>>>>>> parent of e18cbf8... update of the R function jointSurroPenalSimul to take into account the Rmpi command
     !call MPI_FINALIZE(code) 
     !!print*,"suis là================="
     ! fermeture des fichiers des parametres
