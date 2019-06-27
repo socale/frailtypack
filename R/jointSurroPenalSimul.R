8e7fc0bc6c11717eafb4b48c4fc439c44095b412
socale <cs10@PRT-020.DIM-UB2.local>
2019-06-27 17:56:44 +0200

    Revert "yess"
    This reverts commit 279be51cd270e3d5796ee23423fe520cc716747e.

--- R/jointSurroPenalSimul.R
+++ R/jointSurroPenalSimul.R
@@ -903,7 +903,7 @@ jointSurroPenalSimul = function(maxit = 40, indicator.zeta = 1, indicator.alpha 
   mpi.bcast.Robj2slave(affiche.itter)
   mpi.bcast.Robj2slave(vbetast)
   mpi.bcast.Robj2slave(vbetastinit)
-  dyn.load("/gpfs/home/csofeu/R/x86_64-pc-linux-gnu-library/3.5/frailtypack/libs/frailtypack.so")
+  dyn.load("frailtypack/scr/frailtypack.so")
   dyn.load("libmpi.so")
   
   ans <- mpi.remote.exec(funcforMPI(
