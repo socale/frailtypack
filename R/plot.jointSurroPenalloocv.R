a5a24cc8c420b225fbd484ac2d38719c7acf7719
socale <cs10@PRT-020.DIM-UB2.local>
2020-01-07 13:00:03 +0100

    update the function to allows the plot of the results of loocv

--- R/plot.jointSurroPenalloocv.R
+++ R/plot.jointSurroPenalloocv.R
@@ -2,7 +2,7 @@
 ##' canditate surrogate endpoint.
 ##' 
 ##' @description{
-##' Plot of the leave-one-out crossvalidation for the evaluation of the joint surrogate model 
+##' Plot the leave-one-out crossvalidation for the evaluation of the joint surrogate model 
 ##' }
 ##' 
 ##' @aliases plot.jointSurroPenalloocv
@@ -55,79 +55,78 @@
 ##' 
 "plot.jointSurroPenalloocv" <- 
   function(object, unusedtrial = NULL, x = "bottomleft", y = NULL, main = NULL, ...){
-  data.gumbel = object$result
-  data.gumbel <- data.gumbel[!(data.gumbel$trialID %in% unusedtrial),]
-  data.plot <- data.frame(matrix(NA, nrow = 3 * nrow(data.gumbel), ncol = 4))
-  names(data.plot) <- c("trialID", "beta.T", "ordre", "beta")
-  data.plot$ordre <- rep(c(0,1,2),nrow(data.gumbel))
-  
-  # remove from unusedtrial all trials allready in the vector object$notconvtrial  
-  unusedtrial <- unusedtrial[!c(unusedtrial %in% intersect(unusedtrial, object$notconvtrial))]
-  print(unusedtrial)
-  
-  j <- 1
-  for(i in 1:nrow(data.gumbel)){
-    if(data.plot$trialID[i] %in% unusedtrial){
-      data.plot$trialID[j:(j+2)] <- data.gumbel$trial[i]
-      data.plot$beta.T[j:(j+2)] <- 0
-      data.plot$beta[j:(j+2)] <- c(0,0,0)
-    }else{
-      data.plot$trialID[j:(j+2)] <- data.gumbel$trial[i]
-      data.plot$beta.T[j:(j+2)] <- data.gumbel$beta.T[i]
-      data.plot$beta[j:(j+2)] <- as.numeric(data.gumbel[i,c("beta.T.i", "Inf.95.CI", "Sup.95.CI")])
+    data.gumbel = object$result
+    data.gumbel <- data.gumbel[!(data.gumbel$trialID %in% unusedtrial),]
+    data.plot <- data.frame(matrix(NA, nrow = 3 * nrow(data.gumbel), ncol = 4))
+    names(data.plot) <- c("trialID", "beta.T", "ordre", "beta")
+    data.plot$ordre <- rep(c(0,1,2),nrow(data.gumbel))
+    
+    # remove from unusedtrial all trials allready in the vector object$notconvtrial  
+    unusedtrial <- unusedtrial[!c(unusedtrial %in% intersect(unusedtrial, object$notconvtrial))]
+    
+    j <- 1
+    for(i in 1:nrow(data.gumbel)){
+      if(data.plot$trialID[i] %in% unusedtrial){
+        data.plot$trialID[j:(j+2)] <- data.gumbel$trial[i]
+        data.plot$beta.T[j:(j+2)] <- 0
+        data.plot$beta[j:(j+2)] <- c(0,0,0)
+      }else{
+        data.plot$trialID[j:(j+2)] <- data.gumbel$trial[i]
+        data.plot$beta.T[j:(j+2)] <- data.gumbel$beta.T[i]
+        data.plot$beta[j:(j+2)] <- as.numeric(data.gumbel[i,c("beta.T.i", "Inf.95.CI", "Sup.95.CI")])
+      }
+      j = j+3
     }
-    j = j+3
-  }
-  
-  # I allow 0 to all trials belonging in the vector unusedtrial
-  data.plot2 <- data.frame(matrix(NA, nrow = 3 * object$ntrial, ncol = 4))
-  names(data.plot2) <- c("trialID", "beta.T", "ordre", "beta")
-  data.plot2$ordre <- rep(c(0,1,2),object$ntrial)
-  for(i in 1:nrow(data.plot))
+    
+    # I allow 0 to all trials belonging in the vector unusedtrial
+    data.plot2 <- data.frame(matrix(NA, nrow = 3 * object$ntrial, ncol = 4))
+    names(data.plot2) <- c("trialID", "beta.T", "ordre", "beta")
+    data.plot2$ordre <- rep(c(0,1,2),object$ntrial)
+    for(i in 1:nrow(data.plot))
       data.plot2[i,] <- data.plot[i,] 
-  k<-1
-  while (k <= length(unusedtrial)){
-    data.plot2$trialID[j:(j+2)] <- unusedtrial[k]
-    data.plot2$beta.T[j:(j+2)] <- 0
-    data.plot2$beta[j:(j+2)] <- c(0,0,0)
-    j <- j+3
-    k <- k+1
-  }
-  
-  k<-1
-  while (k <= length(object$notconvtrial)){
-    data.plot2$trialID[j:(j+2)] <- object$notconvtrial[k]
-    data.plot2$beta.T[j:(j+2)] <- 0
-    data.plot2$beta[j:(j+2)] <- c(0, 0,0)
-    j <- j+3
-    k <- k+1
-  }
-  
-  doBy::orderBy(~ trialID+ordre,data.plot2)
-  trialnotused <- sort(c(unusedtrial, object$notconvtrial))
-  if(length(trialnotused)>=1) mainlabel <- trialnotused[1]
-  if(length(trialnotused)>1){
-    for(i in 2:length(trialnotused))
-      mainlabel <- paste(mainlabel,trialnotused[i], sep = ", ")
-  }
-  
-  if(is.null(main)){
-    if(length(mainlabel) > 0){ # to avoid to display the main in cases where all trials have been used
+    k<-1
+    while (k <= length(unusedtrial)){
+      data.plot2$trialID[j:(j+2)] <- unusedtrial[k]
+      data.plot2$beta.T[j:(j+2)] <- 0
+      data.plot2$beta[j:(j+2)] <- c(0,0,0)
+      j <- j+3
+      k <- k+1
+    }
+    
+    k<-1
+    while (k <= length(object$notconvtrial)){
+      data.plot2$trialID[j:(j+2)] <- object$notconvtrial[k]
+      data.plot2$beta.T[j:(j+2)] <- 0
+      data.plot2$beta[j:(j+2)] <- c(0, 0,0)
+      j <- j+3
+      k <- k+1
+    }
+    
+    doBy::orderBy(~ trialID+ordre,data.plot2)
+    trialnotused <- sort(c(unusedtrial, object$notconvtrial))
+    if(length(trialnotused)>=1) mainlabel <- trialnotused[1]
+    if(length(trialnotused)>1){
+      for(i in 2:length(trialnotused))
+        mainlabel <- paste(mainlabel,trialnotused[i], sep = ", ")
+    }
+    
+    if(is.null(main)){
+      if(length(mainlabel) > 0){ # to avoid to display the main in cases where all trials have been used
+        boxplot(data.plot2$beta ~ data.plot2$trialID, xlab = "Trials", 
+                ylab = "Log Hazard ration of the true endpoint",
+                main = paste("Unused trials = ", mainlabel, sep = ""))
+      }
+      else{
+        boxplot(data.plot2$beta ~ data.plot2$trialID, xlab = "Trials",
+                ylab = "Log Hazard ration of the true endpoint")
+      }
+    }else{
       boxplot(data.plot2$beta ~ data.plot2$trialID, xlab = "Trials", 
-              ylab = "Log Hazard ratio of the true endpoint",
-              main = paste("Unused trials = ", mainlabel, sep = ""))
+              ylab = "Log Hazard ration of the true endpoint",
+              main = main)
     }
-    else{
-      boxplot(data.plot2$beta ~ data.plot2$trialID, xlab = "Trials",
-              ylab = "Log Hazard ratio of the true endpoint")
-    }
-  }else{
-    boxplot(data.plot2$beta ~ data.plot2$trialID, xlab = "Trials", 
-            ylab = "Log Hazard ratio of the true endpoint",
-            main = main)
+    points(data.plot2$trialID[!(data.plot2$trialID %in% unusedtrial)],data.plot2$beta.T[!(data.plot2$trialID %in% unusedtrial)])
+    legend(x = x, y = y, c("Beta observed", "Beta predict"), cex = 1, pch= c(1,15))
+    abline(h = 0)
+    return(paste("Unused trials = ", mainlabel, sep = ""))
   }
-  points(data.plot2$trialID[!(data.plot2$trialID %in% unusedtrial)],data.plot2$beta.T[!(data.plot2$trialID %in% unusedtrial)])
-  legend(x = x, y = y, c("Beta observed", "Beta predict"), cex = 1, pch= c(1,15))
-  abline(h = 0)
-  return(paste("Unused trials = ", mainlabel, sep = ""))
-}
