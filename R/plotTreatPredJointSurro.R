4aa2734e73ed29afdb28d80bde3b9cea40e7d3c4
socale <cs10@PRT-020.DIM-UB2.local>
2020-02-06 18:27:45 +0100

    bug fixed when consider "HR" instead of "logHR" in the plot

--- R/plotTreatPredJointSurro.R
+++ R/plotTreatPredJointSurro.R
@@ -10,13 +10,15 @@
 #' @usage
 #' plotTreatPredJointSurro(object, from = -2, to = 2, type = "Coef", 
 #'    var.used = "error.estim", alpha. = 0.05, n = 1000, lty = 2, d = 3, 
-#'    colCI = "blue", xlab = "beta.S", ylab = "beta.T.predict")
+#'    colCI = "blue", xlab = "beta.S", ylab = "beta.T.predict, 
+#'    pred.int.use = "up"")
 #' 
 #' @param object An object inheriting from \code{jointSurroPenal} class
 ##' (output from calling the function \code{jointSurroPenal} ).
 #' @param from, The range over which the function will be plotted. The default is \code{from -2 to 2}
 #' @param to 
 #' @param type The type of graphic, \code{"Coef"} for the \code{log HR} or \code{"HR"} for hazard ratio.
+#' If set to \code{HR}, the arguments \code{from} and \code{to} must take positive values.
 #' The default is \code{"Coef"}.
 #' @param var.used This argument can take two values. The first one is \code{"error.estim"}
 ##' and indicates if the prediction error take into account
@@ -37,6 +39,10 @@
 #' @param colCI The color used to display the confidence interval.
 #' @param xlab A title for the x axis.
 #' @param ylab A title for the y axis.
+#' @param pred.int.use A character string that indicates the bound of the prediction interval 
+#' to use to compute the STE. Possible values are \code{up} for the upper bound (the default)
+#' or \code{lw} for the lower bound. \code{up} induces protective treatment effects and \code{lw}
+#' induces risk factors.
 #'
 #' @return For a considered treatment effects on the surrogate enpoint, plot the
 #' associated treatment effects on the true endpoint predicted from the joint surrogate model
@@ -52,6 +58,10 @@
 #' measure for meta-analytic surrogate endpoint validation." Pharmaceutical 
 #' Statistics, 5(3), 173-186.ISSN 1539-1612.
 #' 
+#' Sofeu, C. L. and Rondeau, V. (2020). How to use frailtypack for validating failure-time surrogate 
+#' endpoints using individual patient data from meta-analyses of randomized controlled trials. 
+#' PLOS ONE; 15, 1–25.
+#' @importFrom graphics points curve
 #' @keywords surrogate prediction
 #' @export
 #'
@@ -71,15 +81,34 @@
 #' 
 #' plotTreatPredJointSurro(joint.surro.ovar, from = -2, to = 2, 
 #'                 type = "HR", var.used = "error.estim", lty = 2)
+#'                 
+#' ### For a value of ste greater than 0 (HR > 1), which induces deleterious
+#' ### treatment effet, argument "pred.int.use" can be set to "lw"  
+#' 
+#' plotTreatPredJointSurro(joint.surro.ovar, from = -2, to = 2, 
+#'                 type = "HR", var.used = "error.estim", lty = 2,
+#'                 pred.int.use = "lw")
 #' 
 #' }
 #' 
-plotTreatPredJointSurro= function(object, from = -2, to = 2, type = "Coef", var.used = "error.estim", 
-                                       alpha. = 0.05, n = 1000, lty = 2, d = 3, colCI = "blue", xlab = "beta.S", 
-                                       ylab = "beta.T.predict"){
+plotTreatPredJointSurro <- function(object, from = -2, to = 2, type = "Coef", var.used = "error.estim", 
+                                    alpha. = 0.05, n = 1000, lty = 2, d = 3, colCI = "blue", xlab = "beta.S", 
+                                    ylab = "beta.T.predict", pred.int.use = "up"){
   # type  = "coef" or "HR"
   # n = number of points for the curve
   # colCI = color Confidence interval
+  
+  # data control
+  if((type == "HR") & (to < 0 | from < 0) ){
+    if(to > 0) {
+      warning("With argument 'type' = 'HR', arguments 'to' and 'from' must take positive values")
+      from <- 0
+    }
+    else{
+      stop("With argument 'type' = 'HR', arguments 'to' and 'from' must take positive values")
+    }
+  }
+  
   beta  <- object$beta.t
   dab   <- object$Coefficients$Estimate[nrow(object$Coefficients)-4]
   daa   <- object$Coefficients$Estimate[nrow(object$Coefficients)-6]
@@ -98,13 +127,19 @@ plotTreatPredJointSurro= function(object, from = -2, to = 2, type = "Coef", var.
   
   if(var.used == "error.estim"){
     if(type == "Coef"){ # log HR
-      curve (expr = beta + (dab/daa) * (x - alpha), from = from, to = to, n = n, xlab = xlab, ylab = ylab, 
-             main = paste("STE = ", round(ste(object, var.used = var.used), d), "(HR = ", round(exp(ste(object, var.used = var.used)), d), ")"))
+      expressx <- function(x){
+        beta + (dab/daa) * (x - alpha)
+      }
+      
+      expressxVect <- Vectorize(expressx)
+      curve (expr = expressxVect, from = from, to = to, n = n, xlab = xlab, ylab = ylab, 
+             main = paste("STE = ", round(ste(object, var.used = var.used, pred.int.use = pred.int.use), d), 
+                          "(HR = ", round(exp(ste(object, var.used = var.used, pred.int.use = pred.int.use)), d), ")"))
       #inf
       expressInf <- function(x){
         beta + (dab/daa) * (x - alpha) - qnorm(1-alpha./2) * 
-                    sqrt(t(x1) %*% (Vmu + (((x - alpha)/daa)**2) * VD) %*% x1
-                         + dbb * (1 - R2trial))
+          sqrt(t(x1) %*% (Vmu + (((x - alpha)/daa)**2) * VD) %*% x1
+               + dbb * (1 - R2trial))
       }
       
       expressInfVect <- Vectorize(expressInf)
@@ -120,22 +155,33 @@ plotTreatPredJointSurro= function(object, from = -2, to = 2, type = "Coef", var.
       curve (expr = expressSupVect, from = from, to = to, add = T, col = colCI, n = n, lty = lty)
     }
     else{ # HR
-      curve (expr = exp(beta + (dab/daa) * (x - alpha)), from = from, to = to, n = n, xlab = xlab, ylab = ylab, 
-             main = paste("STE = ", round(ste(object, var.used = var.used), d), "(HR = ", round(exp(ste(object, var.used = var.used)), d), ")"))
+      
+      expressx <- function(x){
+        x <- log(x) # on suppose que les entrees sont des HR et donc on les converti en log HR
+        exp(beta + (dab/daa) * (x - alpha))
+      }
+      
+      expressxVect <- Vectorize(expressx)
+      
+      curve (expr = expressxVect, from = from, to = to, n = n, xlab = xlab, ylab = ylab, 
+             main = paste("STE = ", round(ste(object, var.used = var.used, pred.int.use = pred.int.use), d), 
+                          "(HR = ", round(exp(ste(object, var.used = var.used, pred.int.use = pred.int.use)), d), ")"))
       #inf
       expressInf <- function(x){
+        x <- log(x) # on suppose que les entrees sont des HR et donc on les converti en log HR
         exp(beta + (dab/daa) * (x - alpha) - qnorm(1-alpha./2) * 
-          sqrt(t(x1) %*% (Vmu + (((x - alpha)/daa)**2) * VD) %*% x1
-               + dbb * (1 - R2trial)))
+              sqrt(t(x1) %*% (Vmu + (((x - alpha)/daa)**2) * VD) %*% x1
+                   + dbb * (1 - R2trial)))
       }
       
       expressInfVect <- Vectorize(expressInf)
       curve (expr = expressInfVect, from = from, to = to, add = T, col = colCI, n = n, lty = lty)
       #sup
       expressSup <- function(x){
+        x <- log(x) # on suppose que les entrees sont des HR et donc on les converti en log HR
         exp(beta + (dab/daa) * (x - alpha) + qnorm(1-alpha./2) * 
-          sqrt(t(x1) %*% (Vmu + (((x - alpha)/daa)**2) * VD) %*% x1
-               + dbb * (1 - R2trial)))
+              sqrt(t(x1) %*% (Vmu + (((x - alpha)/daa)**2) * VD) %*% x1
+                   + dbb * (1 - R2trial)))
       }
       
       expressSupVect <- Vectorize(expressSup)
@@ -144,40 +190,77 @@ plotTreatPredJointSurro= function(object, from = -2, to = 2, type = "Coef", var.
   }
   else{
     if(type == "Coef"){ # log HR
-      curve (expr = beta + (dab/daa) * (x - alpha), from = from, to = to, n = n, xlab = xlab, ylab = ylab, 
-             main = paste("STE = ", round(ste(object, var.used = var.used), d), "(HR = ", round(exp(ste(object, 
-                           var.used = var.used)), d), ")"))
+      expressx <- function(x){
+        beta + (dab/daa) * (x - alpha)
+      }
+      
+      expressxVect <- Vectorize(expressx)
+      curve (expr = expressxVect, from = from, to = to, n = n, xlab = xlab, ylab = ylab, 
+             main = paste("STE = ", round(ste(object, var.used = var.used, pred.int.use = pred.int.use), d), 
+                          "(HR = ", round(exp(ste(object, var.used = var.used, pred.int.use = pred.int.use)), d), ")"))
       #inf
-      curve (expr = beta + (dab/daa) * (x - alpha) - qnorm(1-alpha./2) * sqrt(
-        dbb * (1 - R2trial)),
-        from = from, to = to, add = T, col = colCI, n = n, lty = lty)
+      expressInf <- function(x){
+        beta + (dab/daa) * (x - alpha) - qnorm(1-alpha./2) * sqrt(
+          dbb * (1 - R2trial))
+      }
+      
+      expressInfVect <- Vectorize(expressInf)
+      curve (expr = expressInfVect,
+             from = from, to = to, add = T, col = colCI, n = n, lty = lty)
+      
       #sup
-      curve (expr = beta + (dab/daa) * (x - alpha) + qnorm(1-alpha./2) * sqrt(
-        dbb * (1 - R2trial)),
-        from = from, to = to, add = T, col = colCI, n = n, lty = lty)
+      expressSup <- function(x){
+        beta + (dab/daa) * (x - alpha) + qnorm(1-alpha./2) * sqrt(
+          dbb * (1 - R2trial))
+      }
+      
+      expressSupVect <- Vectorize(expressSup)
+      curve (expr = expressSupVect,
+             from = from, to = to, add = T, col = colCI, n = n, lty = lty)
     }
     else{ # HR
-      curve (expr = exp(beta + (dab/daa) * (x - alpha)), from = from, to = to, n = n, xlab = xlab, ylab = ylab, 
-             main = paste("STE = ", round(ste(object, var.used = var.used), d), "(HR = ", round(exp(ste(object, var.used = var.used)), d), ")"))
+      expressx <- function(x){
+        x <- log(x) # on suppose que les entrees sont des HR et donc on les converti en log HR
+        exp(beta + (dab/daa) * (x - alpha))
+      }
+      
+      expressxVect <- Vectorize(expressx)
+      curve (expr = expressxVect, from = from, to = to, n = n, xlab = xlab, ylab = ylab, 
+             main = paste("STE = ", round(ste(object, var.used = var.used, pred.int.use = pred.int.use), d), 
+                          "(HR = ", round(exp(ste(object, var.used = var.used, pred.int.use = pred.int.use)), d), ")"))
       #inf
-      curve (expr = exp(beta + (dab/daa) * (x - alpha) - qnorm(1-alpha./2) * sqrt(
-        dbb * (1 - R2trial))),
-        from = from, to = to, add = T, col = colCI, n = n, lty = lty)
+      expressInf <- function(x){
+        x <- log(x) # on suppose que les entrees sont des HR et donc on les converti en log HR
+        exp(beta + (dab/daa) * (x - alpha) - qnorm(1-alpha./2) * sqrt(
+          dbb * (1 - R2trial)))
+      }
+      
+      expressInfVect <- Vectorize(expressInf)
+      curve (expr = expressInfVect,
+             from = from, to = to, add = T, col = colCI, n = n, lty = lty)
       #sup
-      curve (expr = exp(beta + (dab/daa) * (x - alpha) + qnorm(1-alpha./2) * sqrt(
-        dbb * (1 - R2trial))),
-        from = from, to = to, add = T, col = colCI, n = n, lty = lty)
+      expressSup <- function(x){
+        x <- log(x) # on suppose que les entrees sont des HR et donc on les converti en log HR
+        exp(beta + (dab/daa) * (x - alpha) + qnorm(1-alpha./2) * sqrt(
+          dbb * (1 - R2trial)))
+      }
+      
+      expressSupVect <- Vectorize(expressSup)
+      curve (expr = expressSupVect,
+             from = from, to = to, add = T, col = colCI, n = n, lty = lty)
     }
   }
   
   #ste
   if(type == "HR"){ # log HR
     abline(h = 1, col = "cyan", lty = 4)
-    points(ste(object, var.used = var.used),1)
+    points(exp(ste(object, var.used = var.used, pred.int.use = pred.int.use)),1)
+    abline(v = exp(ste(object, var.used = var.used, pred.int.use = pred.int.use)), col = "cyan", lty = 4)
   }
   else{
     abline(h = 0, col = "cyan", lty = 4)
-    points(ste(object, var.used = var.used),0)
+    points(ste(object, var.used = var.used, pred.int.use = pred.int.use),0)
+    abline(v = ste(object, var.used = var.used, pred.int.use = pred.int.use), col = "cyan", lty = 4)
   }
-  abline(v = ste(object, var.used = var.used), col = "cyan", lty = 4)
+  
 }
