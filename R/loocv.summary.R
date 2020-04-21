cb6f8d528eeddebba792b702b8cca2fb46d61c10
socale <cs10@PRT-020.DIM-UB2.local>
2020-04-21 14:12:44 +0200

    bugg fixed

--- R/loocv.summary.R
+++ R/loocv.summary.R
@@ -3,7 +3,7 @@
 
 loocv.summary <- function(loocv.object, nb.parameters, names.parameters){
   n <- length(loocv.object$different.models)
-  result <- data.frame(matri(0, n, nb.parameters))
+  result <- data.frame(matrix(0, n, nb.parameters))
   names(result) <- names.parameters
   coef <- sapply(1:n, function(i){
     loocv.object$different.models[[i]]$Coefficient[,1]
